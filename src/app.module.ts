db50cc424ed00ca87790a064659129f7f9a3b857
Jakub Kozik <jakubkozik120@gmail.com>
2022-09-20 19:37:50 +0200

    Add mysql and typeorm database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+// Load env file
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.SQL_PASSWORD,
+      database: 'ecommerce',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

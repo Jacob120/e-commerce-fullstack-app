e6b3ad0e07399a5cacc191e4df17fe8ef90e2d3e
Jakub Kozik <jakubkozik120@gmail.com>
2022-09-25 17:46:22 +0200

    Change IconBoxes folder name to FeatureBox

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BannerGroup from '../../features/BannerGroup/BannerGroup';
 import HomeCarousel from '../../features/HomeCarousel/HomeCarousel';
-import FeatureBoxes from '../../features/IconBoxes/FeatureBoxes';
+import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 
 import NavBar from '../../layout/NavBar/NavBar';
 import TopBar from '../../layout/TopBar/TopBar';

170086dcdbbc0ae1a2f8d0b891c3ad77417e94e3
Jakub Kozik <jakubkozik120@gmail.com>
2022-10-05 14:30:39 +0200

    Fix logout

--- client/src/components/views/Logout/Logout.js
+++ client/src/components/views/Logout/Logout.js
@@ -1,28 +1,13 @@
-import { API_URL } from '../../../config';
-import { logOut } from '../../../redux/usersRedux';
-import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const options = {
-      method: 'DELETE',
-    };
-
-    fetch(`${API_URL}auth/logout`, options)
-      .then(() => {
-        sessionStorage.clear();
-        // dispatch(logOut());
-        navigate('/');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [dispatch]);
+    sessionStorage.clear();
+    navigate('/');
+  }, []);
 
   return null;
 };

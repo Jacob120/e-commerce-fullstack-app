65e1887fa8edcab97a681c8db4638436d511cb08
Jakub Kozik <jakubkozik120@gmail.com>
2022-10-11 21:09:34 +0200

    Add Framer Motion in BannerGroup

--- client/src/components/features/BannerGroup/BannerGroup.js
+++ client/src/components/features/BannerGroup/BannerGroup.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const BannerGroup = () => {
   return (
@@ -11,57 +12,79 @@ const BannerGroup = () => {
       <Col md={12} lg={8} className={styles.left_side}>
         <Row xs={12}>
           <Col md={6} className={styles.banner_wrapper}>
-            <Link to="/">
-              <div className={styles.left_banner_text}>
-                <p className="m-0">New Arrivals</p>
-                <h4>
-                  Sneakers &<br /> Athletic Shoes
-                </h4>
-                <Button variant="light">DISCOVER NOW</Button>
-              </div>
-              <img
-                className={'d-block  ' + styles.banner_small}
-                src="./images/banner-group/banner-small-1.jpg"
-                alt="shoes-banner-1"
-              />
-            </Link>
+            <motion.div
+              initial={{ x: -100 }}
+              whileInView={{ x: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.8 }}
+            >
+              <Link to="/">
+                <div className={styles.left_banner_text}>
+                  <p className="m-0">New Arrivals</p>
+                  <h4>
+                    Sneakers &<br /> Athletic Shoes
+                  </h4>
+                  <Button variant="light">DISCOVER NOW</Button>
+                </div>
+                <img
+                  className={'d-block  ' + styles.banner_small}
+                  src="./images/banner-group/banner-small-1.jpg"
+                  alt="shoes-banner-1"
+                />
+              </Link>
+            </motion.div>
           </Col>
           <Col md={6} className={styles.banner_wrapper}>
-            <Link to="/">
-              <div className={styles.right_banner_text}>
-                <p className="m-0">Clearance</p>
-                <h4>
-                  Heels <br /> up to 70% off
-                </h4>
+            <motion.div
+              initial={{ x: -100 }}
+              whileInView={{ x: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.5 }}
+            >
+              <Link to="/">
+                <div className={styles.right_banner_text}>
+                  <p className="m-0">Clearance</p>
+                  <h4>
+                    Heels <br /> up to 70% off
+                  </h4>
 
-                <Button variant="dark">SHOP NOW</Button>
-              </div>
-              <img
-                className={'d-block  ' + styles.banner_small}
-                src="./images/banner-group/banner-small-3.jpg"
-                alt="shoes-banner-2"
-              />
-            </Link>
+                  <Button variant="dark">SHOP NOW</Button>
+                </div>
+                <img
+                  className={'d-block  ' + styles.banner_small}
+                  src="./images/banner-group/banner-small-3.jpg"
+                  alt="shoes-banner-2"
+                />
+              </Link>
+            </motion.div>
           </Col>
         </Row>
+
         <Row className="d-none d-lg-block">
           <Col xs={12} className={styles.banner_wrapper}>
-            <Link to="/">
-              <div className={styles.bottom_banner_text}>
-                <p className="m-0">On Sale</p>
-                <h4>
-                  Athletic Shoes
-                  <br /> up to 30% off
-                </h4>
+            <motion.div
+              initial={{ y: 100 }}
+              whileInView={{ y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.8 }}
+            >
+              <Link to="/">
+                <div className={styles.bottom_banner_text}>
+                  <p className="m-0">On Sale</p>
+                  <h4>
+                    Athletic Shoes
+                    <br /> up to 30% off
+                  </h4>
 
-                <Button variant="light">SHOP NOW</Button>
-              </div>
-              <img
-                className={'d-block ' + styles.banner_left_long}
-                src="./images/banner-group/left-banner-long-3.jpg"
-                alt="shoes-banner-2"
-              />
-            </Link>
+                  <Button variant="light">SHOP NOW</Button>
+                </div>
+                <img
+                  className={'d-block ' + styles.banner_left_long}
+                  src="./images/banner-group/left-banner-long-3.jpg"
+                  alt="shoes-banner-2"
+                />
+              </Link>
+            </motion.div>
           </Col>
         </Row>
       </Col>

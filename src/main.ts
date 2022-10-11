64deac3d806f5a1e30fd994236b5635bd5bce088
Jakub Kozik <jakubkozik120@gmail.com>
2022-10-11 16:31:04 +0200

    Fix heroku

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000/api/products'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders:
-      'Content-Type, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
-  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

5ab8bcfee455b5e163647dfb56f534c7cd9688fe
Jakub Kozik <jakubkozik120@gmail.com>
2022-10-11 17:25:41 +0200

    Change api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api';
